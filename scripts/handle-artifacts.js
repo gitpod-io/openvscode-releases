478b706cdd2aaa24d74b0fec05e10be42efaed6f
Filip Troníček <filip.tronicek@seznam.cz>
2021-08-28 09:21:25 +0000

    Change up the output

--- scripts/handle-artifacts.js
+++ scripts/handle-artifacts.js
@@ -3,10 +3,9 @@ const artifact = require('@actions/artifact');
 const downloadAll = async () => {
   const artifactClient = artifact.create();
   const downloadResponse = await artifactClient.downloadAllArtifacts();
-  // output result
+
   for (response in downloadResponse) {
-    console.log(response.artifactName);
-    console.log(response.downloadPath);
+    console.table(response)
   }
 }
 
